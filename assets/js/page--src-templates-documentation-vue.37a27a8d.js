8ec45e5184feb8c946a16d1f0537be22a82d83b3
serosgb <serosgb@gmail.com>
2020-12-04 21:39:16 -0500

    Updates

---
+++ assets/js/page--src-templates-documentation-vue.37a27a8d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{sad8:function(n,t){},"z/i5":function(n,t,e){"use strict";e.r(t);var u={},a=e("KHd+"),i=e("sad8"),o=e.n(i),s=Object(a.a)(u,(function(){var n=this.$createElement,t=this._self._c||n;return t("div",[t("VueRemarkContent")],1)}),[],!1,null,null,null);"function"==typeof o.a&&o()(s);t.default=s.exports}}]);
